213be1ce1758c8bae9b27cca28d975b5d7fa652a
wdarvin <wesdarvin@gmail.com>
2015-02-24 21:20:47 -0500

    Revert 9bea57e..4a05207
    This rolls back to commit 9bea57edefe4c801e757e46b6ca47e57fc5c50ca.

--- methods.h
+++ methods.h
@@ -5,84 +5,131 @@
 #include <FEHServo.h>
 #include <FEHRPS.h>
 
-//used as motor power for commands
-//if necessary create second for turning
-#define POWER 25 
-
 //initialization statements
 ButtonBoard buttons(FEHIO::Bank3);
+DigitalEncoder rEncoder(FEHIO::P2_0);
+DigitalEncoder lEncoder(FEHIO::P2_1);
 FEHMotor rMotor(FEHMotor::Motor0);
 FEHMotor lMotor(FEHMotor::Motor1);
 
 
+//pulse forward measuring using encoders
+//for use withen the rpsForward command
+void pulseForward(int percent, int counts)
+{
+	int lCounts=0; 
+	int rCounts=0;
+	int	sum=0;
+	//Rest encoder counts
+	rEncoder.ResetCounts();
+	lEncoder.ResetCounts();
+	
+	//set both motors to the desired percen
+	rMotor.SetPercent(percent);
+	lMotor.SetPercent(percent);
+	
+	//move each motor forward the expected number of counts
+	while(sum < counts*2)
+	{
+		lCounts = lEncoder.Counts();
+		rCounts = rEncoder.Counts();
+		sum = lCounts+rCounts;
+	}
+	//turn motors off
+	rMotor.Stop();
+	lMotor.Stop();
+}
+
+//pulse left measuring using encoders
+//for use withen the rpsTurn command
+void pulseLeft(int percent, int counts)
+{
+	int lCounts=0; 
+	int rCounts=0;
+	int	sum=0;
+	//Rest encoder counts
+	rEncoder.ResetCounts();
+	lEncoder.ResetCounts();
+	
+	//set both motors to the desired percent
+	rMotor.SetPercent(percent);
+	lMotor.SetPercent(-percent);
+	
+	//move each motor forward the expected number of counts
+	while(sum < counts*2)
+	{
+		lCounts = lEncoder.Counts();
+		rCounts = rEncoder.Counts();
+		sum = lCounts+rCounts;
+	}
+
+	//turn motors off
+	rMotor.Stop();
+	lMotor.Stop();
+}
+
+//pulse right measuring using encoders
+//for use withen the rpsTurn command
+void pulseRight(int percent, int counts)
+{
+	int lCounts=0; 
+	int rCounts=0;
+	int	sum=0;
+	//Rest encoder counts
+	rEncoder.ResetCounts();
+	lEncoder.ResetCounts();
+	
+	//set both motors to the desired percent
+	rMotor.SetPercent(-percent);
+	lMotor.SetPercent(percent);
+	
+	//move each motor forward the expected number of counts
+	while(sum < counts*2)
+	{
+		lCounts = lEncoder.Counts();
+		rCounts = rEncoder.Counts();
+		sum = lCounts+rCounts;
+	}
+
+	//turn motors off
+	rMotor.Stop();
+	lMotor.Stop();
+}
+
 //use RPS to check x location while robot is facing the +x direction
 void rpsXPlus(float x_coordinate)
 {
-	int change=2;
-	//check whether the robot is within an acceptable range
+//check whether the robot is within an acceptable range
     while(RPS.X() < x_coordinate - .5 || RPS.X() > x_coordinate + .5)
     {
-      if(RPS.X() > x_coordinate)
-      {
-      	//ensure motor never directly changes direction
-      	if(change==1)
-    		{
-    			rMotor.Stop();
-				lMotor.Stop();
-    		}
-      	//set motors in reverse
-      	rMotor.SetPercent(-POWER);
-			lMotor.SetPercent(-POWER);
-			change=0;
-      }
-      else if(RPS.X() < x_coordinate)
-      {
-      	//ensure motor never directly changes direction
-       	if(change==0)
-    		{
-    			rMotor.Stop();
-				lMotor.Stop();
-    		}
-      	//power motors forward
-      	rMotor.SetPercent(POWER);
-			lMotor.SetPercent(POWER); 
-			change=1;    
-      }
+        if(RPS.X() > x_coordinate)
+        {
+        //pulse the motors for a short duration in the correct direction
+        pulseForward(-75,5);
+        }
+        else if(RPS.X() < x_coordinate)
+        {
+        //pulse the motors for a short duration in the correct direction
+        pulseForward(75,5);
+        }
 	}
 }
 
 //use RPS to check x location while robot is facing the -x direction
 void rpsXMinus(float x_coordinate)
 {
-	int change=2;
-	//check whether the robot is within an acceptable range
+//check whether the robot is within an acceptable range
     while(RPS.X() < x_coordinate - .5 || RPS.X() > x_coordinate + .5)
     {
         if(RPS.X() > x_coordinate)
         {
-        	//ensure motor never directly changes direction
-      	if(change==1)
-    		{
-    			rMotor.Stop();
-				lMotor.Stop();
-    		}
-      	//power motors forward
-      	rMotor.SetPercent(POWER);
-			lMotor.SetPercent(POWER);
-			change=0;
+        //pulse the motors for a short duration in the correct direction
+        pulseForward(75,5);
         }
         else if(RPS.X() < x_coordinate)
         {
-        	//ensure motor never directly changes direction
-       	if(change==0)
-    		{
-    			rMotor.Stop();
-				lMotor.Stop();
-    		}
-      	//set motors in reverse
-      	rMotor.SetPercent(-POWER);
-			lMotor.SetPercent(-POWER);
-			change=1;
+        //pulse the motors for a short duration in the correct direction
+        pulseForward(-75,5);
         }
 	}
 }
@@ -91,35 +138,18 @@ void rpsXMinus(float x_coordinate)
 //use RPS to check y location while robot is facing the -y direction
 void rpsYMinus(float y_coordinate)
 {
-	int change=2;
 	//check whether the robot is within an acceptable range
     while(RPS.Y() < y_coordinate - .5 || RPS.Y() > y_coordinate + .5)
     {
         if(RPS.Y() > y_coordinate)
         {
-        //ensure motor never directly changes direction
-      	if(change==1)
-    		{
-    			rMotor.Stop();
-				lMotor.Stop();
-    		}
-      	//power motors forward
-      	rMotor.SetPercent(POWER);
-			lMotor.SetPercent(POWER);
-			change=0;
+        //pulse the motors for a short duration in the correct direction
+        pulseForward(75,5);
         }
         else if(RPS.Y() < y_coordinate)
         {
-        //ensure motor never directly changes direction
-       	if(change==0)
-    		{
-    			rMotor.Stop();
-				lMotor.Stop();
-    		}
-      	//set motors in reverse
-      	rMotor.SetPercent(-POWER);
-			lMotor.SetPercent(-POWER);
-			change=1;
+        //pulse the motors for a short duration in the correct direction
+        pulseForward(-75,5);
         }
 	}
 }
@@ -127,35 +157,18 @@ void rpsYMinus(float y_coordinate)
 //use RPS to check y location while robot is facing the +y direction
 void rpsYPlus(float y_coordinate)
 {
-	int change=2;
 	//check whether the robot is within an acceptable range
     while(RPS.Y() < y_coordinate - .5 || RPS.Y() > y_coordinate + .5)
     {
         if(RPS.Y() > y_coordinate)
         {
-        //ensure motor never directly changes direction
-      	if(change==1)
-    		{
-    			rMotor.Stop();
-				lMotor.Stop();
-    		}
-      	//set motors in reverse
-      	rMotor.SetPercent(-POWER);
-			lMotor.SetPercent(-POWER);
-			change=0;
+        //pulse the motors for a short duration in the correct direction
+        pulseForward(-75,5);
         }
         else if(RPS.Y() < y_coordinate)
         {
-        //ensure motor never directly changes direction
-       	if(change==0)
-    		{
-    			rMotor.Stop();
-				lMotor.Stop();
-    		}
-      	//power motors forward
-      	rMotor.SetPercent(POWER);
-			lMotor.SetPercent(POWER); 
-			change=1; 
+        //pulse the motors for a short duration in the correct direction
+        pulseForward(75,5);
         }
 	}
 }
@@ -168,29 +181,14 @@ void rpsTurn(float heading)
 	{
 		if(RPS.Heading() > heading)
 		{
-			//ensure motor never directly changes direction
-      	if(change==1)
-    		{
-    			rMotor.Stop();
-				lMotor.Stop();
-    		}
-      	//power motor for turn right
-      	rMotor.SetPercent(-POWER);
-			lMotor.SetPercent(POWER);
-			change=0;
+			//pulse the motors for a short duration in the correct direction
+			pulseRight(50,2);
+			//LCD.WriteLine("HEADING CHECK RIGHT");
 		}
 		else if(RPS.Heading() < heading)
 		{
-			//ensure motor never directly changes direction
-       	if(change==0)
-    		{
-    			rMotor.Stop();
-				lMotor.Stop();
-    		}
-      	//power motor for turn left
-      	rMotor.SetPercent(POWER);
-			lMotor.SetPercent(-POWER); 
-			change=1; 
+			pulseLeft(50,2);
+			//LCD.WriteLine("HEADING CHECK LEFT");
       }
    } 
 }
